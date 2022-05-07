6e585a5c3e1f11ca4a6f4471fb188e4b96ec42cf
rfontao <ricardo.fontao@gmail.com>
2022-05-07 01:37:44 +0100

    It just works :) (context switching)

--- kernel/include/task.h
+++ kernel/include/task.h
@@ -1,9 +1,13 @@
 #ifndef TASK_H
 #define TASK_H
 
+#define STACK_SIZE 50
+
 typedef struct {
+  /* Must be first element in struct */
   volatile uint8_t* stackPointer;
-  uint8_t stack[50];
+
+  uint8_t stack[STACK_SIZE];
 
   /* period in ticks */
   int period;
